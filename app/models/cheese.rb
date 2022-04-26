ca623323aa0040e0794e8c2cb1f3daf8c18013f3
Sanna Ishaq <sakinah.ishaq@gmail.com>
2022-04-26 07:15:08 -0400

    commit tree 57492793be0dd5b337f044f89fb099aa6d28dcad

--- app/models/cheese.rb
+++ app/models/cheese.rb
@@ -4,4 +4,14 @@ class Cheese < ApplicationRecord
     "#{self.name}: $#{self.price}"
   end
 
+  def index
+    cheeses = Cheese.all
+    render json: cheeses
+  end 
+
+  def show
+    cheese = Cheese.find(params[:id])
+    render json: cheese
+  end
+  
 end
